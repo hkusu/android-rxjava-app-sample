9f09494e6190992d8fea782c2993eca886f7cd4a
hkusu <kusu0806@gmail.com>
2016-03-11 22:48:09 +0900

    fix bug.

--- app/src/main/java/io/github/hkusu/rxapp/util/Util.java
+++ app/src/main/java/io/github/hkusu/rxapp/util/Util.java
@@ -7,7 +7,7 @@ import io.github.hkusu.rxapp.BuildConfig;
 public class Util {
     public static void logThread(String methodName) {
         if (BuildConfig.DEBUG) {
-            Log.d("RxJava-thread" + methodName, Thread.currentThread().getName());
+            Log.d("RxJava-thread:" + methodName, Thread.currentThread().getName());
         }
     }
 }
